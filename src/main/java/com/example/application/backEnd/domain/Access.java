067c7d18582e68a94b219a7991f0cb212c5e01f1
danilZherdev <106810725+svetli010783@users.noreply.github.com>
2022-08-19 14:35:14 +0300

    /add

--- src/main/java/com/example/application/backEnd/domain/Access.java
+++ src/main/java/com/example/application/backEnd/domain/Access.java
@@ -14,6 +14,7 @@ import java.util.Date;
 @Getter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Access {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
