9b1ed4ade771a11a0ccd9fea02704bf17e5ad259
YanakovAlexey <yanakov006@mail.ru>
2022-08-24 14:03:52 +0300

    /add

--- src/main/java/com/example/application/views/MainLayout.java
+++ src/main/java/com/example/application/views/MainLayout.java
@@ -7,19 +7,20 @@ import com.example.application.views.about.AboutView;
 import com.example.application.views.helloworld.DigitalBooksView;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.applayout.AppLayout;
-import com.vaadin.flow.component.applayout.DrawerToggle;
-import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.messages.MessageInput;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
 
 /**
  * The main view is a top-level placeholder for other views.
  */
+@Route("button-basic")
 public class MainLayout extends AppLayout {
 
     private H1 viewTitle;
@@ -27,7 +28,7 @@ public class MainLayout extends AppLayout {
     public MainLayout() {
         setPrimarySection(Section.DRAWER);
         addToNavbar(true, createHeaderContent());
-        addToDrawer(createDrawerContent());
+//        addToDrawer(createDrawerContent());
 
 
     }
@@ -38,28 +39,52 @@ public class MainLayout extends AppLayout {
         viewTitle = new H1();
         viewTitle.addClassNames("view-title");
 
+
         Header header = new Header(viewTitle);
         header.addClassNames("view-header");
         Icon searchIcon = new Icon(VaadinIcon.SEARCH);
-        Icon userIcon = new Icon(VaadinIcon.USER);
         Icon bookIcon = new Icon(VaadinIcon.OPEN_BOOK);
-        Icon cartIcon = new Icon(VaadinIcon.CART);
+        ButtonBasic cartButton = new ButtonBasic(VaadinIcon.CART);
+        Button userButton = new Button(String.valueOf(VaadinIcon.USER));
         name = new TextField();
-        header.add(name, searchIcon, bookIcon, cartIcon, userIcon);
+        header.add(name, searchIcon, bookIcon, cartButton, userButton);
+
 
 
         return header;
     }
 
-    private Component createDrawerContent() {
-        H2 appName = new H2("DigitalBookBackEnd");
-        appName.addClassNames("app-name");
+//    private Component createDrawerContent() {
+//        H2 appName = new H2("DigitalBookBackEnd");
+//        appName.addClassNames("app-name");
+//
+//        com.vaadin.flow.component.html.Section section = new com.vaadin.flow.component.html.Section(appName,
+//                createNavigation(), createFooter());
+//        section.addClassNames("drawer-section");
+//        return section;
+//    }
 
-        com.vaadin.flow.component.html.Section section = new com.vaadin.flow.component.html.Section(appName,
-                createNavigation(), createFooter());
-        section.addClassNames("drawer-section");
-        return section;
+    public class ButtonBasic extends Div {
+    private int counter = 0;
+
+    public ButtonBasic(VaadinIcon cart) {
+        Button button = new Button(String.valueOf(cart));
+        Paragraph info = new Paragraph(infoText());
+        addClickListener(clickEvent -> {
+            counter += 1;
+            info.setText(infoText());
+        });
+
+        HorizontalLayout horizontalLayout = new HorizontalLayout(button, info);
+        horizontalLayout.setAlignItems(FlexComponent.Alignment.BASELINE);
+        add(horizontalLayout);
     }
+
+    private String infoText() {
+        return String.format("Clicked %d times", counter);
+    }
+
+}
 
     private AppNav createNavigation() {
         // AppNav is not yet an official component.
