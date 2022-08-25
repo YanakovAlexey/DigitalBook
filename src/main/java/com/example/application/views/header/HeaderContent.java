0347aeb5fe9b990a42329d64c00a581778e08872
YanakovAlexey <yanakov006@mail.ru>
2022-08-25 12:33:42 +0300

    /add

---
+++ src/main/java/com/example/application/views/header/HeaderContent.java
@@ -0,0 +1,14 @@
+package com.example.application.views.header;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.icon.Icon;
+
+import static com.vaadin.flow.component.icon.VaadinIcon.*;
+import static com.vaadin.flow.component.icon.VaadinIcon.USER;
+
+public class HeaderContent extends MainLayout{
+
+}
