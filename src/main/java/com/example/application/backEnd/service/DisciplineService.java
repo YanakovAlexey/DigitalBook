30221039e6438fbd7807b4a5eb58fe62a1382c7e
danilZherdev <106810725+svetli010783@users.noreply.github.com>
2022-08-29 12:35:16 +0300

    /correct

---
+++ src/main/java/com/example/application/backEnd/service/DisciplineService.java
@@ -0,0 +1,6 @@
+package com.example.application.backEnd.service;
+
+public interface DisciplineService {
+
+
+}
--- src/main/java/com/example/application/views/content/BookShapeContent.java
+++ src/main/java/com/example/application/views/content/BookShapeContent.java
@@ -1,5 +1,5 @@
 package com.example.application.views.content;
 
-public class BookShapeContent {
+public class  BookShapeContent  {
 
 }
