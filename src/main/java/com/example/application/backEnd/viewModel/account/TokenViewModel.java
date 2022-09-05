fecb84a1101d39f742931c857ddf01770a0cbdc1
danilZherdev <106810725+svetli010783@users.noreply.github.com>
2022-09-05 11:38:43 +0300

    asad

--- src/main/java/com/example/application/backEnd/viewModel/account/TokenViewModel.java
+++ src/main/java/com/example/application/backEnd/viewModel/account/TokenViewModel.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class TokenViewModel {
+public class    TokenViewModel {
     String token;
     LocalDateTime expiredAt;
 
