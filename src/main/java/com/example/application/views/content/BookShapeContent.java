baba76025dd80d6168b7f49bcfcee43f4d0bd11c
IlyaYanakov <104890933+IlyaYanakov@users.noreply.github.com>
2022-09-13 12:24:46 +0300

    /fix

--- src/main/java/com/example/application/views/content/BookShapeContent.java
+++ src/main/java/com/example/application/views/content/BookShapeContent.java
@@ -27,29 +27,10 @@ public class BookShapeContent extends Div {
 
   @Autowired
   public BookShapeContent() {
-    add(constructUI());
+
   }
 
   private Component constructUI() {
-
-    Avatar avatarImage = new Avatar("https://pngicon.ru/file/uploads/dota-2.png");
-
-    VerticalLayout layout = new VerticalLayout();
-
-    Book book = new Book();
-    book.setTitle("Герой");
-    book.setDescription("Книга про одинокого героя");
-    Grid<Book> grid = new Grid<>(Book.class, false);
-    grid.addColumn(Book::getTitle).setHeader("Название");
-    grid.addColumn(Book::getDescription).setHeader("Описание");
-
-    avatarImage.setImage("https://pngicon.ru/file/uploads/dota-2.png");
-    avatarImage.setHeight("140px");
-    avatarImage.setWidth("140px");
-
-//    List<Book> people = DataService.getPeople();
-    grid.setItems(book);
-    layout.add(grid, avatarImage);
-    return layout;
+    return null;
   }
 }
