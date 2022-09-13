2b36b583bdff33f426bfbc1705b1a0f83485ce42
IlyaYanakov <104890933+IlyaYanakov@users.noreply.github.com>
2022-09-13 12:51:30 +0300

    /add changes

--- src/main/java/com/example/application/views/content/BookShapeContent.java
+++ src/main/java/com/example/application/views/content/BookShapeContent.java
@@ -6,17 +6,21 @@ import com.example.application.backEnd.service.BookService;
 import com.example.application.backEnd.viewModel.BookViewModel;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.charts.model.Select;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.gridpro.GridPro;
 import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.spreadsheet.shared.ContentMode;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
+
 
 import java.awt.*;
 import java.util.List;
@@ -27,8 +31,8 @@ public class BookShapeContent extends Div {
 
   @Autowired
   public BookShapeContent() {
-    GridLayout grid = new GridLayout(4, 4);
-
+    GridLayout grid = new GridLayout();
+    grid.addLayoutComponent("Author", );
   }
 
   private Component constructUI() {
