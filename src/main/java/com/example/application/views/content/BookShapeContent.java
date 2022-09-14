61968d2848e1f8bf86f5a624756477e1e91f4ef9
IlyaYanakov <104890933+IlyaYanakov@users.noreply.github.com>
2022-09-14 13:23:49 +0300

    /add

--- src/main/java/com/example/application/views/content/BookShapeContent.java
+++ src/main/java/com/example/application/views/content/BookShapeContent.java
@@ -1,41 +1,27 @@
 package com.example.application.views.content;
 
-import com.example.application.backEnd.domain.Book;
-import com.example.application.backEnd.domain.Users;
-import com.example.application.backEnd.service.BookService;
-import com.example.application.backEnd.viewModel.BookViewModel;
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.avatar.Avatar;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.charts.model.Select;
-import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.gridpro.GridPro;
 import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.spreadsheet.shared.ContentMode;
-import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 import java.awt.*;
-import java.util.List;
-import java.util.stream.Stream;
 
 @Route("shapes")
 public class BookShapeContent extends Div {
 
   @Autowired
   public BookShapeContent() {
-    GridLayout grid = new GridLayout();
-    grid.addLayoutComponent("Author", );
+    
   }
 
-  private Component constructUI() {
-    return null;
-  }
+//  private Image constructUI() {
+//    var image = new Image(book.getBookImg(), book.getTitle() + " cover");
+//    image.setHeight("70px");
+//    image.addClickListener(event -> System.out.println("Должна быть ссылка на книгу"));
+//    return image;
+//  }
 }
