fb7fef1bf54a3fecae2b3707020dc03e2e4e4e5e
YanakovAlexey <yanakov006@mail.ru>
2022-09-22 15:17:49 +0300

    /add

--- src/main/java/com/example/application/views/MainLayout.java
+++ src/main/java/com/example/application/views/MainLayout.java
@@ -2,10 +2,7 @@ package com.example.application.views;
 
 import com.example.application.backEnd.builder.BookBuilder;
 import com.example.application.backEnd.service.BookService;
-import com.example.application.views.authorization.AuthorizationView;
 import com.example.application.views.content.BookShapeContent;
-import com.example.application.views.registration.RegistrationView;
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLayout;
@@ -19,22 +16,25 @@ import javax.annotation.security.RolesAllowed;
  */
 @Route("/")
 @AnonymousAllowed
-public class MainLayout extends VerticalLayout implements RouterLayout {
+public class MainLayout extends VerticalLayout  implements RouterLayout {
 
     private final BookService bookService;
     private final BookBuilder bookBuilder;
-    private final HeaderView headerView = new HeaderView();
-    //    private final FooterView footerView = new FooterView();
-    private Component contentView;
 
-    public MainLayout(BookService bookService,
-                      BookBuilder bookBuilder) {
+    private final HeaderView headerView = new HeaderView();
+//    private final FooterView footerView = new FooterView();
+    private final BookShapeContent contentView;
+
+    public MainLayout(BookService bookService, BookBuilder bookBuilder) {
         this.bookService = bookService;
         this.bookBuilder = bookBuilder;
         this.setPadding(false);
         this.setHeight("100%");
-        contentView = new ContentView();
-        add(headerView, contentView);
+        contentView = new BookShapeContent(bookService, bookBuilder);
+
+        add(headerView);
+        add(contentView);
+//        add(footerView);
     }
 }
 
