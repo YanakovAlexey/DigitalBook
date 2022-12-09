2639750d5c13d3bfca7179c38e21288837a04ecd
YanakovAlexey <yanakov006@mail.ru>
2022-12-09 15:17:26 +0300

    correct css

--- src/main/java/com/example/application/backEnd/viewModel/BasketPositionViewModel.java
+++ src/main/java/com/example/application/backEnd/viewModel/BasketPositionViewModel.java
@@ -1,7 +1,5 @@
 package com.example.application.backEnd.viewModel;
 
-import com.example.application.backEnd.domain.Basket;
-import com.example.application.backEnd.domain.Book;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
