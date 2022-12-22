91315d751ab783c68ea514e8b1ab5cf2e88305a7
YanakovAlexey <yanakov006@mail.ru>
2022-12-22 13:16:18 +0300

    fix

--- src/main/java/com/example/application/backEnd/service/ResponseException.java
+++ src/main/java/com/example/application/backEnd/service/ResponseException.java
@@ -1,11 +1,11 @@
 package com.example.application.backEnd.service;
 
-public class ResponseException extends Exception{
+public class ResponseException extends Exception {
     public String error;
     public String message;
     public int code;
 
-    public ResponseException(String error, String message, int code){
+    public ResponseException(String error, String message, int code) {
         this.error = error;
         this.message = message;
         this.code = code;
